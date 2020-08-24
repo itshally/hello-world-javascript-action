dc7f5ebd24816950ef2721b06aa55087fe673e22
Hally <hbianan@gmail.com>
2020-08-24 01:44:29 -0400

    file added

--- index.js
+++ index.js
@@ -2,7 +2,20 @@ const core = require('@actions/core'),
       github = require('@actions/github');
 let fs = require('fs');
 
-const htmlLine = `
+try{
+    //"who-to-greet" input defined in action metadata file
+    const nameToGreet = core.getInput('who-to-greet')
+    console.log(`hello ${nameToGreet}`)
+
+    const time = (new Date()).toTimeString()
+    core.setOutput('time', time)
+
+    // get the JSON webhook payload for the event that triggered the workflow
+
+    const payload = JSON.stringify(github.context.payload, undefined, 2)
+    console.log(`the event payload: ${payload}`)
+
+    const htmlLine = `
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -20,20 +33,6 @@ fs.writeFile('index.html', htmlLine, err => {
     if(err) throw err;
     console.log('index.html file is created successfully!')
 })
-      
-
-try{
-    //"who-to-greet" input defined in action metadata file
-    const nameToGreet = core.getInput('who-to-greet')
-    console.log(`hello ${nameToGreet}`)
-
-    const time = (new Date()).toTimeString()
-    core.setOutput('time', time)
-
-    // get the JSON webhook payload for the event that triggered the workflow
-
-    const payload = JSON.stringify(github.context.payload, undefined, 2)
-    console.log(`the event payload: ${payload}`)
 }catch(error){
     core.setFailed(error.message)
 }
